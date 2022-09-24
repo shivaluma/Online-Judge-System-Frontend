bc7c0d725fc5980a3327975a3861c01729a0fae3
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2022-09-25 02:43:10 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // https://judgeonline.df.r.appspot.com/api/
 // http://localhost:3003/api/
 const instance = axios.create({
-  baseURL: `https://judgeonline.df.r.appspot.com/api/`,
+  baseURL: `https://api.shivaluma.dev/judge/api/`,
 });
 
 instance.interceptors.request.use(
